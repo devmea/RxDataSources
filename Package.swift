c72eecf5d412b9234366696039bbb7083d51ede0
Eduardo Dias <eduardo.dias@360mea.com>
2025-01-21 16:57:40 +0400

    Update RxSwift to 6.7.0

--- Package.swift
+++ Package.swift
@@ -1,24 +1,59 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10.0
 
 import PackageDescription
 
 let package = Package(
   name: "RxDataSources",
   platforms: [
-    .iOS(.v9), .tvOS(.v9)
+    .iOS(.v14),
+    .tvOS(.v14)
   ],
   products: [
-    .library(name: "RxDataSources", targets: ["RxDataSources"]),
-    .library(name: "RxDataSources-Dynamic", type: .dynamic, targets: ["RxDataSources"]),
-    .library(name: "Differentiator", targets: ["Differentiator"])
+    .library(
+        name: "RxDataSources",
+        targets: [
+            "RxDataSources"
+        ]
+    ),
+    .library(
+        name: "RxDataSources-Dynamic",
+        type: .dynamic,
+        targets: [
+            "RxDataSources"
+        ]
+    ),
+    .library(
+        name: "Differentiator",
+        targets: [
+            "Differentiator"
+        ]
+    )
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
+    .package(
+        url: "https://github.com/ReactiveX/RxSwift.git",
+        exact: Version(stringLiteral: "6.7.0")
+    ),
   ],
   targets: [
-    .target(name: "RxDataSources", dependencies: ["Differentiator", "RxSwift", "RxCocoa"]),
+    .target(
+        name: "RxDataSources",
+        dependencies: [
+            "Differentiator",
+            "RxSwift",
+            .product(
+                name: "RxCocoa",
+                package: "RxSwift"
+            )
+        ]
+    ),
     .target(name: "Differentiator"),
-    .testTarget(name: "RxDataSourcesTests", dependencies: ["RxDataSources"])
+    .testTarget(
+        name: "RxDataSourcesTests",
+        dependencies: [
+            "RxDataSources"
+        ]
+    )
   ],
   swiftLanguageVersions: [.v5]
 )
